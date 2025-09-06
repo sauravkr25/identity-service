9bc652195ae2845238c100dddcd65d270829fc98
sauravkr25 <90563802+sauravkr25@users.noreply.github.com>
2025-09-06 23:08:18 +0530

    minor changes

--- src/main/java/com/shareride/identity/config/CachedBodyHttpServletRequestWrapper.java
+++ src/main/java/com/shareride/identity/config/CachedBodyHttpServletRequestWrapper.java
@@ -1,14 +1,15 @@
 package com.shareride.identity.config;
 
-import jakarta.servlet.ReadListener;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import org.springframework.util.StreamUtils;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class CachedBodyHttpServletRequestWrapper extends HttpServletRequestWrapper {
@@ -23,33 +24,12 @@ public class CachedBodyHttpServletRequestWrapper extends HttpServletRequestWrapp
 
     @Override
     public ServletInputStream getInputStream() {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
 
-        return new ServletInputStream() {
-            private final InputStream cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
-
-            @Override
-            public int read() throws IOException {
-                return cachedBodyInputStream.read();
-            }
-
-            @Override
-            public boolean isFinished() {
-                try {
-                    return cachedBodyInputStream.available() == 0;
-                } catch (IOException e) {
-                    return true;
-                }
-            }
-
-            @Override
-            public boolean isReady() {
-                return false;
-            }
-
-            @Override
-            public void setReadListener(ReadListener readListener) {
-
-            }
-        };
+    @Override
+    public BufferedReader getReader() throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
     }
 }
